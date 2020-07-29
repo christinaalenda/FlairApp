bff26c0865c520c27905b6bef5386f21f9a7c4a2
Christina Lenda <clenda041@gmail.com>
2020-07-29 10:37:47 -0700

    07/29/2020 10:38am EST

--- FlairApp/ViewController.swift
+++ FlairApp/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hello World")
         // Do any additional setup after loading the view.
     }
 
