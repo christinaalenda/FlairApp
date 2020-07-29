17b4147a4e198956c82b7799864e90d586f663f0
Christina Lenda <clenda041@gmail.com>
2020-07-29 10:31:35 -0700

    Initial Commit

---
+++ FlairApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlairApp
+//
+//  Created by Apple on 7/29/20.
+//  Copyright © 2020 Christina Lenda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
